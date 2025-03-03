7556dbbb9c04b7cf8d105bed13a1d528616fed88
ShahFaisalIslam <faisal.islam.ceme@gmail.com>
2025-03-03 18:25:36 +0500

    Added functionality to read and display units for selected quantity, and kept Hi as unit for area

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # GIAIC Project 1 - Unit Convertor
 
 Replicates [Google's Unit Convertor](https://www.google.com/search?q=unit+convertor)
+
+## Installation
+
+1. Install uv in your device
+2. Run `uv sync`
--- main.py
+++ main.py
@@ -8,3 +8,12 @@ st.write("Replicates [Google's Unit Convertor](https://www.google.com/search?q=u
 with open("data/quantity.json") as quantity_file:
     quantity_list = json.loads(quantity_file.read())
     quantity = st.selectbox("Quantity",quantity_list,index=quantity_list.index("Length"),label_visibility="hidden")
+    # Turn quantity into lowercase and replace spaces with underscores
+    quantity = quantity.lower().replace(" ","_")
+    # For every quantity, there exists a file "data/quantity/{quantity}.json"
+    # Open the file for the selected quantity and fetch from it the list of units
+    with open(f"data/quantity/{quantity}.json") as unit_file:
+        unit_list = json.loads(unit_file.read())
+        unit = st.selectbox("Unit",unit_list,label_visibility="hidden")
+        # Turn unit into lowercase and replace spaces with underscores
+        unit = unit.lower().replace(" ","_")
