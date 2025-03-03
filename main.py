a09512428f93caca2bd7fdbb6311ed8cc0b7f893
ShahFaisalIslam <faisal.islam.ceme@gmail.com>
2025-03-03 23:30:10 +0500

    Replaced area with length, and added to json conversion to meters  factor as well as what operator to use with the factor

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ import streamlit as st
 import json
 # Page Setup
 st.set_page_config(page_title="Unit Convertor",layout="wide")
-st.header("Unit Convertor")
+st.header("Unit Convertor",divider=True)
 st.write("Replicates [Google's Unit Convertor](https://www.google.com/search?q=unit+convertor)")
+
+def swap_if_duplicate(**kwargs):
+    if kwargs["direction"] == "left":
+        # Change right unit to previous left unit if it matches the left unit
+        if st.session_state.right_unit == st.session_state.left_unit:
+            st.session_state.right_unit = st.session_state.prev_left_unit
+    else:
+        # Change left unit to previous right unit if it matches the right unit
+        if st.session_state.left_unit == st.session_state.right_unit:
+            st.session_state.left_unit = st.session_state.prev_right_unit
 
 with open("data/quantity.json") as quantity_file:
     quantity_list = json.loads(quantity_file.read())
     quantity = st.selectbox("Quantity",quantity_list,index=quantity_list.index("Length"),label_visibility="hidden")
-    # Turn quantity into lowercase and replace spaces with underscores
     quantity = quantity.lower().replace(" ","_")
-    # For every quantity, there exists a file "data/quantity/{quantity}.json"
-    # Open the file for the selected quantity and fetch from it the list of units
     with open(f"data/quantity/{quantity}.json") as unit_file:
-        unit_list = json.loads(unit_file.read())
-        unit = st.selectbox("Unit",unit_list,label_visibility="hidden")
-        # Turn unit into lowercase and replace spaces with underscores
-        unit = unit.lower().replace(" ","_")
+        unit_data = json.loads(unit_file.read())
+        unit_list = [unit[0] for unit in unit_data]
+        # Three columns, one for left hand side unit, one for equals to, and one for right hand side unit
+        [left_col,_,right_col] = st.columns(3)
+        # Left hand side unit
+        with left_col:
+            st.number_input("Left Value",key="left_value",step=0.01,label_visibility="hidden",value=float(1))
+            st.session_state.prev_left_unit = st.selectbox("Unit",unit_list,label_visibility="hidden",key="left_unit",on_change=swap_if_duplicate,kwargs={"direction":"left"})
+        # Equals to sign
+        with _:
+            st.html("<h1 style='text-align:center'>=</p>")
+        # Right hand side unit
+        with right_col:
+            st.number_input("Right Value",key="right_value",step=0.01,label_visibility="hidden")
+            st.session_state.prev_right_unit = st.selectbox("Unit",unit_list,index=1,label_visibility="hidden",key="right_unit",on_change=swap_if_duplicate,kwargs={"direction":"right"})
